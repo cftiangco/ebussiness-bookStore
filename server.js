77e777103ded940efb4f78fe34d6beabbc4d6376
Crimson Tiangco <cftiangco@gmail.com>
2019-01-14 00:41:11 +0800

    deployment

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const Product = require('./routes/product.route');
 const Admin = require('./routes/admin.route');
 
 //mongoose connection middleware
-mongoose.connect('mongodb://localhost:27017/bookstore', {useNewUrlParser: true});
+if(process.ENV.NODE_ENV === 'production') {
+    mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds255794.mlab.com:55794/bookstore-prod', {useNewUrlParser: true});
+} else {
+    mongoose.connect('mongodb://localhost:27017/bookstore', {useNewUrlParser: true});
+}
+
+
 
 //ejs middleware
 app.set('view engine', 'ejs');
@@ -32,7 +38,7 @@ app.get('/products/contact', (req,res) => {
 app.use('/products',Product);
 app.use('/admin',Admin);
 
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server Running at Port ${PORT}`);
